d3c78f2c6c0d5587d4214b635f9f7233c42b01b4
Pottemuld <32219655+Pottemuld@users.noreply.github.com>
2018-10-11 11:55:47 +0200

    Initial work on connecting client to server

---
+++ SYNchatClient/src/Connection/Client.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Connection;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Pottemuld
+ */
+public class Client {
+
+    
+    InetAddress ip;
+    int port = 8080;
+
+    public Client() {
+    this.ip = ip;
+    this.port = port;
+    
+    }
+
+System.out.println("Connecting to " + ip + " on port " + port);
+
+
+
+
+
+
+}
+
+
