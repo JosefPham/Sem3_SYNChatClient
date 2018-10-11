4b3620f714088c3b63ff6eafd05834987ee7327d
Pottemuld <32219655+Pottemuld@users.noreply.github.com>
2018-10-11 12:46:54 +0200

    SOMEONECONNECTED!

--- SYNchatClient/src/Connection/Client.java
+++ SYNchatClient/src/Connection/Client.java
@@ -5,10 +5,13 @@
  */
 package Connection;
 
+import java.io.IOException;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.net.ssl.SSLSocket;
+import javax.net.ssl.SSLSocketFactory;
 
 /**
  *
@@ -21,12 +24,30 @@ public class Client {
     int port = 8080;
 
     public Client() {
-    this.ip = ip;
+        try {
+            this.ip = (InetAddress) InetAddress.getByName("10.126.37.220");
+        } catch (UnknownHostException ex) {
+            Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, ex);
+        }
     this.port = port;
     
     }
-
-System.out.println("Connecting to " + ip + " on port " + port);
+    
+    public void connectToServer(){
+        try {
+            System.out.println("Connecting to " + ip + " on port " + port + "");
+            SSLSocketFactory factory = (SSLSocketFactory) SSLSocketFactory.getDefault();
+            SSLSocket sslSocket = (SSLSocket) factory.createSocket("10.126.37.220", port);
+        } catch (IOException ex) {
+            Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, ex);
+        }
+               
+    } 
+        
+    public static void main(String[] args) {
+        Client client = new Client();
+        client.connectToServer();
+    }
 
 
 
