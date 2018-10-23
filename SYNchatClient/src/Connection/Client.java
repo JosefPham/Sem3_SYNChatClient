2c810575df52815f8de20161d7cbb0b2b340341c
Staerdahl <peterandersen14@live.dk>
2018-10-23 11:49:13 +0200

    inout/output

--- SYNchatClient/src/Connection/Client.java
+++ SYNchatClient/src/Connection/Client.java
@@ -56,8 +56,12 @@ public class Client{
             System.out.println("Connecting to " + ip + " on port " + port + "");
             this.serverSocket = new Socket(ip,port);
             console = new DataInputStream(System.in);
-            input = new ObjectInputStream(serverSocket.getInputStream());
+            System.out.println("Made console");
             output = new ObjectOutputStream(serverSocket.getOutputStream());
+            System.out.println("Got output");
+            input = new ObjectInputStream(serverSocket.getInputStream());
+            System.out.println("Got Inout");
+            
         } catch (IOException ex) {
             Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, ex);
         }
