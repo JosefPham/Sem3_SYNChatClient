10bdb56ff77cb2cca9291e6f44add1cc9e8bd032
Staerdahl <peterandersen14@live.dk>
2018-10-23 10:22:10 +0200

    reverted cleanup

--- SYNchatClient/src/Connection/Client.java
+++ SYNchatClient/src/Connection/Client.java
@@ -21,7 +21,7 @@ import java.util.logging.Logger;
  *
  * @author Pottemuld
  */
-public class Client {
+public class Client{
 
     Socket serverSocket;
     InetAddress ip;
@@ -29,28 +29,35 @@ public class Client {
     private DataInputStream console;
     private DataInputStream input;
     private DataOutputStream output;
-    private Thread readMessage, sendMessage;
 
     public Client() {
         try {
             this.ip = (InetAddress) InetAddress.getByName("10.126.33.99");
-
+            
         } catch (UnknownHostException ex) {
             Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, ex);
         }
         this.port = port;
-
+        
         connectToServer();
-
+        
         startPublicThreads();
+        
+     //   startPrivateThreads();
+        
+        
+        
+        
+       // listener = new Thread(this);
+       // listener.start();
+        
 
-        //   startPrivateThreads();
     }
 
     public void connectToServer() {
         try {
             System.out.println("Connecting to " + ip + " on port " + port + "");
-            this.serverSocket = new Socket(ip, port);
+            this.serverSocket = new Socket(ip,port);
             console = new DataInputStream(System.in);
             input = new DataInputStream(new BufferedInputStream(serverSocket.getInputStream()));
             output = new DataOutputStream(new BufferedOutputStream(serverSocket.getOutputStream()));
@@ -59,138 +66,140 @@ public class Client {
         }
 
     }
-
-    public void startPrivateThreads() {
-
+    
+    
+    
+    
+    public void startPrivateThreads(){
+            
         Scanner scan = new Scanner(System.in);
-
-        sendMessage = new Thread(new Runnable() {
+        
+        Thread sendMessage = new Thread(new Runnable() {
             @Override
             public void run() {
-                while (true) {
-                    try {
-                        InetAddress local = InetAddress.getLocalHost();
-                        String msg = scan.nextLine();
-                        try {
-
-                            output.writeUTF(":" + local + "   " + msg);
-
-                            //     System.out.println("Sending");
-                            output.flush();
-                        } catch (IOException ex) {
-                            ex.printStackTrace();
-                        }
-                    } catch (UnknownHostException ex) {
-                        Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, ex);
-                    } finally {
-                        sendMessage.interrupt();
-
-                        try {
-                            output.close();
-                        } catch (IOException ex) {
-                            ex.printStackTrace();
-                        }
-                    }
-                }
-            }
-        });
-
-        readMessage = new Thread(new Runnable() {
-            @Override
-            public void run() {
+                while(true){ 
+            try { 
+                InetAddress local = InetAddress.getLocalHost();
+                String msg = scan.nextLine();
                 try {
-                    while (true) {
-                        //    System.out.println("HEllo");
-                        String text = input.readUTF();
-                        System.out.println(text);
-                    }
-                } catch (Exception e) {
-                } finally {
-                    readMessage.interrupt();
-
-                    try {
-                        input.close();
-                    } catch (IOException ex) {
-                        ex.printStackTrace();
-                    }
-                }
+                    
+                    output.writeUTF(":" + local + "   " + msg);
+                    
+                    //     System.out.println("Sending");
+                    output.flush();
+                } catch (IOException ex) {
+                    ex.printStackTrace();
+                } // Sæt vores tråd til null ved finally
+            } catch (UnknownHostException ex) {
+                        Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, ex);
+            } // Sæt vores tråd til null ved finally
+        }
             }
         });
-
+        
+        
+        
+        
+        Thread readMessage = new Thread(new Runnable() {
+            @Override
+            public void run() {
+        try {
+            while(true){
+            //    System.out.println("HEllo");
+                String text = input.readUTF(); 
+                System.out.println(text);
+            }
+        } catch (Exception e) {
+        } finally{
+             // Sæt vores tråd til null
+            try{
+                output.close();
+            } catch (IOException ex) {
+                ex.printStackTrace ();
+            }
+        }
+            }
+        });
+        
+        
         sendMessage.start();
         readMessage.start();
     }
     
     
-    
-
-    public void startPublicThreads() {
-
+    public void startPublicThreads(){
+         
         Scanner scan = new Scanner(System.in);
-
-        sendMessage = new Thread(new Runnable() {
+        
+     
+        
+        Thread sendMessage = new Thread(new Runnable() {
             @Override
             public void run() {
-                while (true) {
-                    try {
-                        InetAddress local = InetAddress.getLocalHost();
-                        String msg = scan.nextLine();
-                        try {
-
-                            output.writeUTF(":" + local + "   " + msg);
-
-                            //     System.out.println("Sending");
-                            output.flush();
-                        } catch (IOException ex) {
-                            ex.printStackTrace();
-                        }
-                    } catch (UnknownHostException ex) {
-                        Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, ex);
-                    } finally {
-                        sendMessage.interrupt();
-
-                        try {
-                            output.close();
-                        } catch (IOException ex) {
-                            ex.printStackTrace();
-                        }
-                    }
-                }
-            }
-        });
-
-        readMessage = new Thread(new Runnable() {
-            @Override
-            public void run() {
+                while(true){ 
+            try { 
+                InetAddress local = InetAddress.getLocalHost();
+                String msg = scan.nextLine();
                 try {
-                    while (true) {
-                        //    System.out.println("HEllo");
-                        String text = input.readUTF();
-                        System.out.println(text);
-                    }
-                } catch (Exception e) {
-                } finally {
-                    readMessage.interrupt();
-
-                    try {
-                        input.close();
-                    } catch (IOException ex) {
-                        ex.printStackTrace();
-                    }
-                }
+                    
+                    output.writeUTF(local + "   " + msg);
+                    
+                    //     System.out.println("Sending");
+                    output.flush();
+                } catch (IOException ex) {
+                    ex.printStackTrace();
+                } // Sæt vores tråd til null ved finally
+            } catch (UnknownHostException ex) {
+                        Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, ex);
+            } // Sæt vores tråd til null ved finally
+        }
             }
         });
-
+        
+        
+        
+        
+        Thread readMessage = new Thread(new Runnable() {
+            @Override
+            public void run() {
+        try {
+            while(true){
+            //    System.out.println("HEllo");
+                String text = input.readUTF(); 
+                System.out.println(text);
+            }
+        } catch (Exception e) {
+        } finally{
+         // Sæt vores tråd til null
+            try{
+                output.close();
+            } catch (IOException ex) {
+                ex.printStackTrace ();
+            }
+        }
+            }
+        });
+        
+        
         sendMessage.start();
         readMessage.start();
-
+        
     }
-
-    public static void main(String[] args) {
-
-        Client client = new Client();
-
-        //   client.chat();
-    }
-
+ 
+          public static void main(String[] args) {
+        
+              
+              
+              Client client = new Client();
+       
+        
+        
+        
+     //   client.chat();
+ 
+    }   
+ 
+    
+    
+  
 }
