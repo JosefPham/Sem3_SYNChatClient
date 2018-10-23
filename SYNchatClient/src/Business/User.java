16577fb3a02fc9cecd5b9c027f38ddb9fec5dc0a
CasaRol <alexander-rol@hotmail.com>
2018-10-23 09:57:52 +0200

    commit

--- SYNchatClient/src/Business/User.java
+++ SYNchatClient/src/Business/User.java
@@ -1,67 +1,7 @@
-<<<<<<< HEAD
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-=======
->>>>>>> master
 package Business;
 
 import Acquaintance.IUser;
 import java.util.List;
-
-<<<<<<< HEAD
-/**
- *
- */
-public class User extends IUser {
-    
-    String email;
-    String password;
-    String firstName;
-    String lastName;
-    int userID;
-    boolean isBanned;
-    int reports;
-    List<Chat> chats;
-
-    public User(String firstName, String lastName, String email, String passwordString) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.email = email;
-        this.password = passwordString;
-    }
-
-    public User(int userID, String firstName, String lastName, String email, String password, boolean isBanned, int reports, List<Chat> chats) {
-        this.userID = userID;
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.email = email;
-        this.password = password;
-        this.isBanned = isBanned;
-        this.reports = reports;
-        this.chats = chats;
-    }
-    
-//    public boolean registerNewUser(String firstName, String lastName, String email, String password) {
-//        if(*mail is not already registered*){
-//            if true, register user
-//            User currentUser = new User(firstName, lastName, email, password);
-//        }
-//        *Call server in order to create userID and such*
-//        
-//        When server has registered the user with all info return true
-//        return true;
-//        }
-//        else {
-//            return false;
-//        }
-    }
-
-
-=======
 
 public class User implements IUser{
 
@@ -97,4 +37,3 @@ public class User implements IUser{
     }
     
 }
->>>>>>> master
