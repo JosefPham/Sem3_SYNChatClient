c9dd341727f6d4e24190f5b4a33d105e5d9e9fe7
SigurdEEspersen <32567367+SigurdEEspersen@users.noreply.github.com>
2018-10-23 11:53:22 +0200

    imports

--- SYNchatClient/src/Presentation/RegisterNewUserController.java
+++ SYNchatClient/src/Presentation/RegisterNewUserController.java
@@ -7,11 +7,9 @@ package Presentation;
 
 import java.io.IOException;
 import java.net.URL;
-import java.util.ArrayList;
 import java.util.ResourceBundle;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
