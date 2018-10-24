40652bb91bce1a33b58c65871643520f8e232fc4
Staerdahl <peterandersen14@live.dk>
2018-10-24 12:13:13 +0200

    Ilogin

--- SYNchatClient/src/Acquaintance/ILogin.java
+++ SYNchatClient/src/Acquaintance/ILogin.java
@@ -1,14 +1,20 @@
 package Acquaintance;
 
-public interface ILogin {
+import java.io.Serializable;
+
+public interface ILogin extends Serializable {
 
     public String gethMail();
 
     public String gethPW();
-    
+
     public int getLoginvalue();
-    
+
     public IUser getUser();
-    
+
+    public void setLoginvalue(int loginvalue);
+
+    public void setUser(IUser user);
+
     public int login(int loginValue);
 }
