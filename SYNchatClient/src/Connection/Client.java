0c764142b5c444fe4051aec3daf40f699a55115c
CasaRol <alexander-rol@hotmail.com>
2018-10-24 11:15:22 +0200

    djgfkhgj

--- SYNchatClient/src/Connection/Client.java
+++ SYNchatClient/src/Connection/Client.java
@@ -42,7 +42,7 @@ public class Client {
         this.port = port;
 
         //outcomment
-        ILogin login = new Login("Hej@Peter.dk", "12345678");
+//        ILogin login = new Login("Hej@Peter.dk", "12345678");
 
         connectToServer();
 //        sendLogin(login);
