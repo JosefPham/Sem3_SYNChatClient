8084ad6e02df9dca4fc6e6ee72487ac9bfb8c1bf
CasaRol <alexander-rol@hotmail.com>
2018-10-25 10:39:02 +0200

    Registration now handles differetn errors

--- SYNchatClient/src/Presentation/RegisterNewUserController.java
+++ SYNchatClient/src/Presentation/RegisterNewUserController.java
@@ -87,36 +87,66 @@ public class RegisterNewUserController implements Initializable {
 
     @FXML
     public void registerNewUser(ActionEvent Event) {
+        if (validateInfo()) {
+//                        PresentationFacade.getInstance().regUser((txt_lastName.getText() + ", " + txt_firstName.getText()), txt_email.getText(), txt_Password1.getText())
+
+            label_warninginfo.setText("Registration Complete");
+//                            backToLoginHandler(Event);
+
+        }
+
+        }
+
+        @FXML
+        public void backToLoginHandler
+        (ActionEvent event
+        
+            ) {
+        try {
+                Parent login = FXMLLoader.load(getClass().getResource("Login.fxml"));
+                Scene newScene = new Scene(login);
+                Stage appStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+                appStage.setScene(newScene);
+                appStage.show();
+            } catch (IOException ex) {
+                Logger.getLogger(LoginController.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+
+    
+
+    private boolean validateInfo() {
+
+        boolean check = false;
         if (!txt_firstName.getText().isEmpty() && !txt_lastName.getText().isEmpty() && !txt_email.getText().isEmpty() && !txt_Password1.getText().isEmpty() && !txt_password2.getText().isEmpty()) {
-            if (txt_Password1.getText().equalsIgnoreCase(txt_password2.getText())) {
-                if (txt_Password1.getText().length() >= 8) {
-                    label_warninginfo.setText("");
-                    if (PresentationFacade.getInstance().regUser((txt_lastName.getText() + ", " + txt_firstName.getText()), txt_email.getText(), txt_Password1.getText())) {
-                    //success change scene to login with confirmation message
+            if (txt_email.getText().contains("@") && txt_email.getText().contains(".")) {
+                if (txt_Password1.getText().equals(txt_password2.getText())) {
+                    if (txt_Password1.getText().length() >= 8) {
+                        if (!choice_Country.getValue().toString().equals("Select Country")) {
+                            check = true;
+                            if (check) {
+//                        PresentationFacade.getInstance().regUser((txt_lastName.getText() + ", " + txt_firstName.getText()), txt_email.getText(), txt_Password1.getText())
+                                return true;
+                            } else {
+                                label_warninginfo.setText("Mail already registred");
+                            }
+                        } else {
+                            label_warninginfo.setText("Please select your country");
+                        }
                     } else {
-                        label_warninginfo.setText("Mail already registred");
+                        label_warninginfo.setText("Password must be atleast 8 characters");
                     }
                 } else {
-                    label_warninginfo.setText("Password must be atleast 8 characters");
+                    label_warninginfo.setText("Passwords does not match");
                 }
             } else {
-                label_warninginfo.setText("Passwords does not match");
+                label_warninginfo.setText("Invalid email");
             }
         } else {
             label_warninginfo.setText("Please fill out the information");
         }
+        // delete when done
+        return false;
     }
 
-    @FXML
-    public void cancelRegistration(ActionEvent event) {
-        try {
-            Parent login = FXMLLoader.load(getClass().getResource("Login.fxml"));
-            Scene newScene = new Scene(login);
-            Stage appStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-            appStage.setScene(newScene);
-            appStage.show();
-        } catch (IOException ex) {
-            Logger.getLogger(LoginController.class.getName()).log(Level.SEVERE, null, ex);
-        }
-    }
 }
